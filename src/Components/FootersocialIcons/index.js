4aa5642d1b9abd03613d666fb9d2bafc15449e54
vashniroylgs <vashniroy.nallapu@labyrinthglobalsolutions.com>
2024-01-04 16:48:35 +0530

    changed twitter icon

--- src/Components/FootersocialIcons/index.js
+++ src/Components/FootersocialIcons/index.js
@@ -4,7 +4,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { FaYoutube } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaInstagramSquare } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
+
 import { FaFacebookF } from "react-icons/fa";
 
 const SocialMediaButtons = () => {
@@ -30,7 +30,16 @@ const SocialMediaButtons = () => {
         >
           <div className="contact-page-social-icon-tooltip">Twitter</div>
           <span>
-            <FaTwitter className="contact-page-social-icon" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-twitter-x"
+              viewBox="0 0 16 16"
+            >
+              <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
+            </svg>
           </span>
         </a>
       </div>
